1aebce5f1e4253e9e8186cfa7a5c394bba8fa31c
BorntoDev <kittikorn.psk@gmail.com>
2021-11-19 13:26:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
-app.get("/products")
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World !! I'm Product");
+});
 
-app.get("/", (req,res) =>{
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello World !! I'm Product 1");
+});
 
-    res.render('index',{username: 'Prameza55+', customers: ["Kitti113","Kittikorn","Kitty"]});
+app.use("/products", productRouter)
+
+app.get("/", (req, res) => {
+
+    res.render('index', { username: 'Prameza55+', customers: ["Kitti113", "Kittikorn", "Kitty"] });
 
 })
 
-app.listen(PORT, ()=>{
-    debug("Listening on port" + chalk.red(" : "+PORT));
+app.listen(PORT, () => {
+    debug("Listening on port" + chalk.red(" : " + PORT));
 })
