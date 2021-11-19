200b30ca9c5d08ccf3135c9de3fd41669898fd42
BorntoDev <kittikorn.psk@gmail.com>
2021-11-19 13:14:12 +0700

    Reverse

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 4000;
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine", "ejs")
 
 app.get("/", (req,res) =>{
 
