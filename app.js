aee3c8dea3de31a0f31106bf2f850ef3960fc25c
BorntoDev <kittikorn.psk@gmail.com>
2021-11-19 13:09:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 4000;
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
-app.set("views","./src/views");
-app.set("view engine", "ejs")
 
 app.get("/", (req,res) =>{
 
