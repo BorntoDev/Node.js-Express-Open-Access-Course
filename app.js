5d8b109ef6f63a5893030cbaf2d3cc18e2c5d614
BorntoDev <kittikorn.psk@gmail.com>
2021-11-19 13:49:04 +0700

    Product Added

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
 productRouter.route("/").get((req, res) => {
-    res.render("products");
+    res.render("products",{
+        products: [
+            {productTitle: 'น้ำยาล้างจาน', productDescription: 'น้ำยาล้างจานสูตร 1 ดีเลิศ', productPrice: 45},
+            {productTitle: 'น้ำยาล้างจาน 2', productDescription: 'น้ำยาล้างจานสูตร 2 ดีเลิศ', productPrice: 65},
+            {productTitle: 'น้ำยาล้างจาน 3', productDescription: 'น้ำยาล้างจานสูตร 3 ดีเลิศ', productPrice: 35},
+            {productTitle: 'น้ำยาล้างจาน 4', productDescription: 'น้ำยาล้างจานสูตร 4 ดีเลิศ', productPrice: 55},
+        ],
+    });
 });
 
 productRouter.route("/1").get((req, res) => {
