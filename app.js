e257549a45809fb669435d6a6692a8fd64141e2d
BorntoDev <kittikorn.psk@gmail.com>
2021-12-01 15:28:29 +0700

    Added Products.json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const products = require("./data/products.json");
 const productRouter = express.Router();
 
 const app = express();
@@ -16,12 +17,7 @@ app.set("view engine", "ejs")
 
 productRouter.route("/").get((req, res) => {
     res.render("products",{
-        products: [
-            {productTitle: 'น้ำยาล้างจาน', productDescription: 'น้ำยาล้างจานสูตร 1 ดีเลิศ', productPrice: 45},
-            {productTitle: 'น้ำยาล้างจาน 2', productDescription: 'น้ำยาล้างจานสูตร 2 ดีเลิศ', productPrice: 65},
-            {productTitle: 'น้ำยาล้างจาน 3', productDescription: 'น้ำยาล้างจานสูตร 3 ดีเลิศ', productPrice: 35},
-            {productTitle: 'น้ำยาล้างจาน 4', productDescription: 'น้ำยาล้างจานสูตร 4 ดีเลิศ', productPrice: 55},
-        ],
+        products,
     });
 });
 
