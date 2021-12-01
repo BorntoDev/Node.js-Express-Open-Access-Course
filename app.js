e32e2a788187a9ff41aa987905a7dac2addff80e
BorntoDev <kittikorn.psk@gmail.com>
2021-12-01 15:33:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
 productRouter.route("/").get((req, res) => {
-    res.render("products",
+    res.render("products",{
         products,
-    );
+    });
 });
 
 productRouter.route("/1").get((req, res) => {
